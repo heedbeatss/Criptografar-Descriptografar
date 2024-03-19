30f637a719f8ad39b127cde6bf70d265ac3c7346
HeeD <140123483+heedbeatss@users.noreply.github.com>
2024-03-19 14:09:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+  const formEncrypt = document.querySelector("#form");
+  formEncrypt.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+  const btn_cripto = document.querySelector("#btn_cripto");
+  btn_cripto.addEventListener("click", () => {
+    const textValue = String(formEncrypt.querySelector("#textarea").value);
+    if (textValue == "") {
+      return alert("Obrigatorio colocar uma palavra para criptografar");
+    }
+    encrypt(textValue);
+  });
+
+  const btn_decripto = document.querySelector("#btn_decripto");
+  btn_decripto.addEventListener("click", () => {
+    const textValue = String(formEncrypt.querySelector("#textarea").value);
+    if (textValue == "") {
+      return alert("Obrigatorio colocar uma palavra para descriptografar");
+    }
+    decrypt(textValue);
+  });
+
+  function encrypt(textValue) {
+    const result = document.querySelector(".result");
+    let arrayText = textValue.split("");
+    let encryptText;
+    encryptText = arrayText
+      .join("")
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+
+    result.innerHTML += ` ${encryptText}`;
+  }
+
+  function decrypt(textValue) {
+    const result = document.querySelector(".result");
+    let arrayText = textValue.split("");
+    let encryptText;
+    encryptText = arrayText
+      .join("")
+      .replace(/ai/g, "a")
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+
+    result.innerHTML += ` ${encryptText}`;
+
+    console.log(arrayText);
+  }
+  
+document.querySelector('#btn__copy').addEventListener('click', function (){
+  var textoAlert = document.getElementById("paragrafo__result").innerHTML
+  alert ("Texto copiado com sucesso!", textoAlert)
+  navigator.clipboard.writeText(textoAlert);
+}  
+);
+
